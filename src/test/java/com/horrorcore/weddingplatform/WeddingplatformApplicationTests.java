2e8aa7bd08740ac7613eaacb951c7be6c9ec7382
Matthew Jeshua Martin <matthew-martin@programmer.net>
2025-02-02 16:36:56 -0500

    Initial Commit

---
+++ src/test/java/com/horrorcore/weddingplatform/WeddingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horrorcore.weddingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
